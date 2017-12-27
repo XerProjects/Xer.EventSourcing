4310526692a1bc21d11101ef6a38cd72f231c62d
jeyjeyemem <joeljeremy.marquez@gmail.com>
2017-12-28 01:02:51 +0800

    Initial commit. Moved from CQRS repository.

---
+++ Src/Internals/TaskUtility.cs
@@ -0,0 +1,21 @@
+﻿namespace System.Threading.Tasks
+{
+    internal static class TaskUtility
+    {
+        internal static readonly Task CompletedTask = Task.FromResult(true);
+
+        internal static Task FromException(Exception ex)
+        {
+            TaskCompletionSource<bool> taskCompletionSource = new TaskCompletionSource<bool>();
+            taskCompletionSource.TrySetException(ex);
+            return taskCompletionSource.Task;
+        }
+
+        internal static Task<TResult> FromException<TResult>(Exception ex)
+        {
+            TaskCompletionSource<TResult> taskCompletionSource = new TaskCompletionSource<TResult>();
+            taskCompletionSource.TrySetException(ex);
+            return taskCompletionSource.Task;
+        }
+    }
+}
